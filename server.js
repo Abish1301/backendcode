508115b6264a296a5770ac7ffc9c5cdd58e16c3f
Abish K <abish@baytech.in>
2024-11-28 18:46:55 +0530

    authmiddleware in server.js

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 const bodyParser = require('body-parser');  
 const { log } = require('console');
 const cors = require('cors');
+const { authenticateToken } = require('./middleware/authMiddleware');
 require('dotenv').config();
+
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json()); 
@@ -19,11 +22,19 @@ app.use((req, res, next) => {
 
 fs.readdirSync('./routes/').forEach(file => {
   if (file.endsWith('.js')) {
-      const routePath = '/api/' + file.split('.')[0];
-      app.use(routePath, require('./routes/' + file));
-      console.log(`API route loaded: ${routePath}`);
+    const routePath = '/api/' + file.split('.')[0];
+    const routeModule = require('./routes/' + file);
+    if (routePath === '/api/auth') {
+      app.use(routePath, routeModule); 
+    } else {
+      // app.use(routePath, authenticateToken, routeModule);
+      app.use(routePath, routeModule);
+
+    }
+    console.log(`API route loaded: ${routePath}`);
   }
 });
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Endpoint not found' });
