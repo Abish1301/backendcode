e0dd405e73f77b8609fd626a6384814671d79179
41*294dd <dheeba@baytech.in>
2024-12-18 12:41:50 +0530

    task/site category

--- routes/workCategory.js
+++ routes/workCategory.js
@@ -8,11 +8,12 @@ const searchableFields = ['name'];
 const field = ['name'];
 
 router.route('/')
-  .post(crudController.getAllByCondition(WorkCategory, searchableFields, workCategoryAttributes))
-  .put(crudController.updateByID(WorkCategory,field, workCategoryAttributes))
+  .post(crudController.getAllByCondition(WorkCategory, searchableFields, workCategoryAttributes,includeModels = [], filter={ type: '1' }))
+  .put(crudController.updateByID(WorkCategory, field, workCategoryAttributes))
   .delete(crudController.deleteRecord(WorkCategory));
+router.post("/task", crudController.getAllByCondition(WorkCategory, searchableFields, workCategoryAttributes, includeModels = [], filter={ type: '2' }))
 
-  router.post("/create", crudController.createWODuplicates(WorkCategory, field, workCategoryAttributes));
-  router.post("/getById", crudController.getAllById(WorkCategory, workCategoryAttributes));
+router.post("/create", crudController.createWODuplicates(WorkCategory, field, workCategoryAttributes));
+router.post("/getById", crudController.getAllById(WorkCategory, workCategoryAttributes));
 
 module.exports = router;
