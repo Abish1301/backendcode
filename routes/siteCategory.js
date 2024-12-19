c55ee9fdb0991a4ee02aab153c1c9d878a19b5ac
Abish K <abish@baytech.in>
2024-12-19 17:34:35 +0530

    final commit

---
+++ routes/siteCategory.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { WorkCategory } = require('../models');
+const crudController = require('../controllers/crudController');
+const { workCategoryAttributes } = require('../utils');
+
+const searchableFields = ['name'];
+const field = ['name'];
+
+router.route('/')
+  .post(crudController.getAllByCondition(WorkCategory, searchableFields, workCategoryAttributes,includeModels = [], filter={ type: '2' }))
+  .put(crudController.updateByID(WorkCategory, field, workCategoryAttributes))
+  .delete(crudController.deleteRecord(WorkCategory));
+
+router.post("/create", crudController.createWODuplicates(WorkCategory, field, workCategoryAttributes));
+router.post("/getById", crudController.getAllById(WorkCategory, workCategoryAttributes));
+
+module.exports = router;
